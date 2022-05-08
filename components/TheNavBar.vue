cf1e7d9255051671066d83fb8bc89946d57b822c
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-05-08 13:20:05 +0500

    Add files via upload

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
       <a class="navbar-brand" href="#">
         <img
@@ -23,14 +23,16 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active nav__link" aria-current="page" href="#"
+              >Home</a
+            >
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link nav__link" href="#">Link</a>
           </li>
           <li class="nav-item dropdown">
             <a
-              class="nav-link dropdown-toggle"
+              class="nav-link dropdown-toggle nav__link"
               href="#"
               id="navbarDropdown"
               role="button"
--- components/TheTranslate.vue
+++ components/TheTranslate.vue
@@ -1,45 +1,69 @@
 <template>
   <div class="translate">
-    <div class="block-translate d-flex">
-      <TheOption />
+    <div class="block-language d-flex justify-content-between">
+      <div class="block-language__text d-flex">
+        <p class="language-text">Из</p>
+        <TheOption @newLanguageCheked="newLanguageOneUpdate" />
+      </div>
+      <div class="block-language__text d-flex">
+        <p class="language-text">В</p>
+        <TheOption @newLanguageCheked="newLanguageTwoUpdate" />
+      </div>
+    </div>
+    <div class="block-translate d-flex justify-content-between mt-2">
       <div class="block-translate-child">
         <TheAreaTranslate @create="translate" />
       </div>
-      <div class="block-translate-child">
-        <p class="translate-text">1</p>
+      <div class="block-translate-child" @click="addText">
+        <p class="translate-text">{{ readyText }}</p>
       </div>
     </div>
   </div>
 </template>
 <script>
-import TheOption from "./TheOption.vue";
 export default {
   data() {
     return {
-      textTranslate: ""
+      textTranslate: "",
+      newLanguageOne: "",
+      newLanguageTwo: "",
+      readyText: "",
     };
   },
   methods: {
     translate(text) {
-      console.log(text); //Текст компонента
+      this.textTranslate = text;
     },
-    runApi() {
+    addText() {
       const options = {
         method: "POST",
         headers: {
+          "content-type": "application/json",
           "X-RapidAPI-Host": "deep-translate1.p.rapidapi.com",
-          "X-RapidAPI-Key": "76d7e06148msh263d850659d08bdp1c483cjsnf26e619a4a8c"
-        }
+          "X-RapidAPI-Key":
+            "76d7e06148msh263d850659d08bdp1c483cjsnf26e619a4a8c",
+        },
+        body: `{"q":"${this.textTranslate}","source":"${this.newLanguageOne}","target":"${this.newLanguageTwo}"}`,
       };
-      fetch("https://deep-translate1.p.rapidapi.com/language/translate/v2/detect", options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    }
-  },
-  components: { TheOption }
-}
 
+      fetch(
+        "https://deep-translate1.p.rapidapi.com/language/translate/v2",
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          this.readyText = response.data.translations.translatedText;
+        })
+        .catch((err) => console.error(err));
+    },
+    newLanguageOneUpdate(newwVal) {
+      this.newLanguageOne = newwVal;
+    },
+    newLanguageTwoUpdate(newwVal) {
+      this.newLanguageTwo = newwVal;
+    },
+  },
+};
 </script>
 
 <style>
